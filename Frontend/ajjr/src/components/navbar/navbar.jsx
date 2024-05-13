c5883ab643a7c2a6f0ee00029e6a493b23203d02
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-05-13 16:11:08 +0200

    navbar setup with img

--- Frontend/ajjr/src/components/navbar/navbar.jsx
+++ Frontend/ajjr/src/components/navbar/navbar.jsx
@@ -3,42 +3,44 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import "../navbar/navbar.css"; // Importerar CSS från samma mapp
+import { useState } from "react";
+import logoImg from "../../images/ajjrlogo.png"; // Gå upp två nivåer, sedan in i images-mappen
 
 function BasicExample() {
+  const [isNavDropdownOpen, setIsNavDropdownOpen] = useState(false);
+
   return (
-    <Navbar expand="lg" className="navbar">
+    <Navbar expand="lg" className="navbar-custom">
       <Container className="">
-        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img src={logoImg} alt="Logotyp" className="logoimg " />{" "}
+          {/* Använd den importerade bilden här */}
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          {/* Använder ms-auto på Nav-komponenten för att skjuta allt till höger */}
           <Nav className="ms-auto">
-            <Nav.Link className="navlink" href="#home">
+            <Nav.Link href="#home" className="nav-link-custom">
               Hem
             </Nav.Link>
             <NavDropdown
-              className="navlink"
               title="Produkter"
               id="basic-nav-dropdown"
+              show={isNavDropdownOpen}
+              onMouseEnter={() => setIsNavDropdownOpen(true)}
+              onMouseLeave={() => setIsNavDropdownOpen(false)}
             >
-              <NavDropdown.Item className="navlink" href="#action/3.1">
-                Musk
-              </NavDropdown.Item>
-              <NavDropdown.Item className="navlink" href="#action/3.1">
-                Oud
-              </NavDropdown.Item>
-              <NavDropdown.Item className="navlink" href="#action/3.2">
-                Parfym
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Musk</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Oud</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Parfym</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link className="navlink" href="#link">
+            <Nav.Link href="#link" className="nav-link-custom">
               Om oss
             </Nav.Link>
-            <Nav.Link className="navlink" href="#link">
+            <Nav.Link href="#link" className="nav-link-custom">
               Kontakta oss
             </Nav.Link>
           </Nav>
