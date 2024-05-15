bc340f19d02a9c8ba1d80bab76b35239c310c0e0
ShakerNasser <shaker.nasser@outlook.com>
2024-05-15 14:23:39 +0200

    aboutus images and styling

--- Frontend/ajjr/src/pages/aboutus.jsx
+++ Frontend/ajjr/src/pages/aboutus.jsx
@@ -3,27 +3,26 @@ import React from 'react';
 const AboutUs = () => {
   return (
     <>
-   
-      <div>
+      <div className='.text-center'>
           <h1> Om Ajjr </h1>
       </div>
       
       <div>
-        <img src="" alt="" />
+      <img src="src/images/descriptionimg.png" alt="sf" />
           <h3>Vår vision</h3>
-          <p></p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae nihil hic delectus excepturi ipsam reprehenderit iusto rem, quam, repellendus accusantium culpa reiciendis sit dolorum aut aperiam a architecto. Fuga, sit.</p>
       </div>
           
       <div>
-        <img src="" alt="" />
+      <img src="src/images/landingpicture.JPG" alt="sf" />
           <h3>Ajjr historia</h3>
-          <p></p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae nihil hic delectus excepturi ipsam reprehenderit iusto rem, quam, repellendus accusantium culpa reiciendis sit dolorum aut aperiam a architecto. Fuga, sit.</p>
       </div>    
           
       <div>
-        <img src="" alt="" />
+      <img src="src/images/produktpageimg.JPG" alt="sf" />
           <h3>Välgörenhet</h3>
-          <p></p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae nihil hic delectus excepturi ipsam reprehenderit iusto rem, quam, repellendus accusantium culpa reiciendis sit dolorum aut aperiam a architecto. Fuga, sit.</p>
       </div>
 
       <div>
@@ -32,7 +31,6 @@ const AboutUs = () => {
       </div>
 
 </>
-
 
   );
 };
