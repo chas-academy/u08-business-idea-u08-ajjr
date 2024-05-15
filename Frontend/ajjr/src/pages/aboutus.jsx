00f3fda7d0cda674e9add173a548cef400e97bb9
ShakerNasser <shaker.nasser@outlook.com>
2024-05-15 12:42:38 +0200

    aboutus

---
+++ Frontend/ajjr/src/pages/aboutus.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <>
+   
+      <div>
+          <h1> Om Ajjr </h1>
+      </div>
+      
+      <div>
+        <img src="" alt="" />
+          <h3>Vår vision</h3>
+          <p></p>
+      </div>
+          
+      <div>
+        <img src="" alt="" />
+          <h3>Ajjr historia</h3>
+          <p></p>
+      </div>    
+          
+      <div>
+        <img src="" alt="" />
+          <h3>Välgörenhet</h3>
+          <p></p>
+      </div>
+
+      <div>
+        <h3>Avslutande text</h3>
+          <p>Tack för att ni handlar från oss. Tillsammans kan vi bidra för en bättre framtid. </p>
+      </div>
+
+</>
+
+
+  );
+};
+
+export default AboutUs;
