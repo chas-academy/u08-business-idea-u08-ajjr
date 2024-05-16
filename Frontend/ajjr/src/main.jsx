44962df887a4a03e90b34e1c1d909108ebe04af2
ArianAFK <arianfekri@hotmail.com>
2024-05-16 21:27:03 +0200

    routing started, FAQ fixes, navbar fixes, other small fixes

--- Frontend/ajjr/src/main.jsx
+++ Frontend/ajjr/src/main.jsx
@@ -3,21 +3,64 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
-import Navbar from './/components/navbar/Navbar.jsx'; // Se till att stigen är korrekt
+import Navigation from './components/navbar/Navbar.jsx'; // Se till att stigen är korrekt
 import './index.css'
+import { BrowserRouter, RouterProvider, createBrowserRouter, Route, Link } from 'react-router-dom';
+import AboutUs from './pages/aboutus/Aboutus.jsx';
+import Perfumes from './pages/perfumes/Perfumes.jsx';
+import FAQ from './pages/faq/Faq.jsx';
+import Login from './pages/login/Login.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "produkter",
+        element: <Perfumes></Perfumes>,
+        children: [
+          {
+            path: "herr",
+            element: <Perfumes></Perfumes>
+          }
+        ]
+      },
+      {
+        path: "omajjr",
+        element: <AboutUs></AboutUs>
+      },
+      {
+        path: "kontakt",
+        element: <AboutUs></AboutUs>
+      },
+      {
+        path: "faq",
+        element: <FAQ></FAQ>
+      },
+      {
+        path: "loggain",
+        element: <Login></Login>
+      }
+
+    ]
+
+
+  },
+
+
+])
 
 // Skapa en React-root för appen
 const appRoot = ReactDOM.createRoot(document.getElementById('root'));
 appRoot.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router}></RouterProvider>
 );
 
-// Skapa en separat React-root för navbaren
+/* // Skapa en separat React-root för navbaren
 const navbarRoot = ReactDOM.createRoot(document.getElementById('navbar-root'));
 navbarRoot.render(
   <React.StrictMode>
     <Navbar />
   </React.StrictMode>
-);
+); */
