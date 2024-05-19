7bdd350774fc47b3450183e9cbaf50380576dcd7
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-05-19 22:28:27 +0200

    skapat en login funktion som ska hantera anrop till databas

--- Frontend/ajjr/src/pages/login/login.jsx
+++ Frontend/ajjr/src/pages/login/login.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+// import React from 'react'
 
-function Login() {
-    return (
-        <div>Login</div>
-    )
+// function Login() {
+//     return (
+//         <div>Login</div>
+//     )
+// }
+
+// export default Login
+import React, { useState } from 'react';
+import "./login.css"
+
+function LoginPage() {
+  const [isResettingPassword, setIsResettingPassword] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
+  const users = [
+    { email: 'example@example.com', password: 'password123' }
+  ];
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const user = users.find(user => user.email === email);
+    if (!user) {
+      setError('Inget konto finns med den angivna e-postadressen.');
+      setSuccess('');
+    } else if (user.password !== password) {
+      setError('Fel lösenord angivet.');
+      setSuccess('');
+    } else {
+      setError('');
+      setSuccess('Du är inloggad!');
+    }
+  };
+
 }
 
-export default Login
+export default LoginPage;
