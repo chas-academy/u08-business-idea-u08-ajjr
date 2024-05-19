2e37d43e3a5ebaf3d563ece0850b4999a4e88613
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-05-19 22:40:06 +0200

    Rensat tomma ytor

--- Frontend/ajjr/src/pages/login/login.jsx
+++ Frontend/ajjr/src/pages/login/login.jsx
@@ -144,8 +144,8 @@ function LoginPage() {
                   className="btn btn-link w-100"
                   onClick={() => setIsResettingPassword(false)}
                 >
-                  Gå tillbaka  
-                </button> //går tillbaka till loginsidan 
+                  Gå tillbaka
+                </button> //går tillbaka till loginsidan
               )}
             </form>
           </div>
