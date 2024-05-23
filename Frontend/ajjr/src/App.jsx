cb7990e21c8c9c2fbc3cbc4da94029539ed29cc3
Brianosumba <144438563+Brianosumba@users.noreply.github.com>
2024-05-23 12:48:35 +0200

    Footer fix

--- Frontend/ajjr/src/App.jsx
+++ Frontend/ajjr/src/App.jsx
@@ -1,43 +1,40 @@
-
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import FAQ from "./pages/faq/faq";
 import "./App.css";
-import Footer from "./components/Footer";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
     <>
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            {}
-            {}
-            <li>
-              <Link to="/faq">FAQ</Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="main-content">
-          <Routes>
-            {}
-            {}
-            <Route path="/faq" element={<FAQ faqData={faqData} />} />
-          </Routes>
-
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              {}
+              {}
+              <li>
+                <Link to="/faq">FAQ</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className="main-content">
+            <Routes>
+              {}
+              {}
+              <Route path="/faq" element={<FAQ faqData={faqData} />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
- <Footer></Footer>
-</>
+      </Router>
+      <Footer></Footer>
+    </>
   );
 };
 
 const faqData = [
-  { question: 'Har ni en butik?', answer: 'Ja, Chas academy lokal 7' },
-  { question: 'Vad är Musk Oud?', answer: 'Parfym från Mekka' },
+  { question: "Har ni en butik?", answer: "Ja, Chas academy lokal 7" },
+  { question: "Vad är Musk Oud?", answer: "Parfym från Mekka" },
 ];
-
 
 export default App;
