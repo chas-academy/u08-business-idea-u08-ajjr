e3aa1d2334f9e4dfd44acf6d9132fb0b0a334d27
Heval Demirel <hevalzzz@hotmail.com>
2024-05-28 15:08:43 +0200

    Kundservice klar

--- Frontend/ajjr/src/pages/customerservice/customerservice.jsx
+++ Frontend/ajjr/src/pages/customerservice/customerservice.jsx
@@ -1,44 +1,59 @@
 import React from 'react';
-import { Container, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import './customerservice.css';
 
 const CustomerService = () => {
   return (
-    <Container>
-      <Col>
-        <h2>Leveransinformation</h2>
-        <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
-            similique ea harum. Porro harum cum nam expedita culpa, ipsum at
-            impedit blanditiis illum iste nostrum quia vel nisi. Laborum at
-            accusantium nostrum cupiditate praesentium tempore qui vitae
-            neque? Fugiat aspernatur deleniti assumenda, at voluptates
-            eligendi adipisci et cum aut accusantium.
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
-            similique ea harum. Porro harum cum nam expedita culpa, ipsum at
-            impedit blanditiis illum iste nostrum quia vel nisi. Laborum at
-            accusantium nostrum cupiditate praesentium tempore qui vitae
-            neque? Fugiat aspernatur deleniti assumenda, at voluptates
-            eligendi adipisci et cum aut accusantium.</p>
-      </Col>
-      <br />
-      <Col>
-        <h2>Retur & Byten</h2>
-        <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
-            similique ea harum. Porro harum cum nam expedita culpa, ipsum at
-            impedit blanditiis illum iste nostrum quia vel nisi. Laborum at
-            accusantium nostrum cupiditate praesentium tempore qui vitae
-            neque? Fugiat aspernatur deleniti assumenda, at voluptates
-            eligendi adipisci et cum aut accusantium.
-            
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
-            similique ea harum. Porro harum cum nam expedita culpa, ipsum at
-            impedit blanditiis illum iste nostrum quia vel nisi. Laborum at
-            accusantium nostrum cupiditate praesentium tempore qui vitae
-            neque? Fugiat aspernatur deleniti assumenda, at voluptates
-            eligendi adipisci et cum aut accusantium.</p>
-      </Col>
+    <Container className="customer-service-container">
+      <Row className="justify-content-center">
+        <Col md={10} className="mb-5">
+          <Card className="service-card">
+            <Card.Body>
+              <Card.Title>Leveransinformation</Card.Title>
+              <Card.Text>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
+                similique ea harum. Porro harum cum nam expedita culpa, ipsum at
+                impedit blanditiis illum iste nostrum quia vel nisi. Laborum at
+                accusantium nostrum cupiditate praesentium tempore qui vitae
+                neque? Fugiat aspernatur deleniti assumenda, at voluptates
+                eligendi adipisci et cum aut accusantium.
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
+                similique ea harum. Porro harum cum nam expedita culpa, ipsum at
+                impedit blanditiis illum iste nostrum quia vel nisi. Laborum at
+                accusantium nostrum cupiditate praesentium tempore qui vitae
+                neque? Fugiat aspernatur deleniti assumenda, at voluptates
+                eligendi adipisci et cum aut accusantium.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col md={10} className="mb-5">
+          <Card className="service-card">
+            <Card.Body>
+              <Card.Title>Retur & Byten</Card.Title>
+              <Card.Text>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
+                similique ea harum. Porro harum cum nam expedita culpa, ipsum at
+                impedit blanditiis illum iste nostrum quia vel nisi. Laborum at
+                accusantium nostrum cupiditate praesentium tempore qui vitae
+                neque? Fugiat aspernatur deleniti assumenda, at voluptates
+                eligendi adipisci et cum aut accusantium.
+                
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
+                similique ea harum. Porro harum cum nam expedita culpa, ipsum at
+                impedit blanditiis illum iste nostrum quia vel nisi. Laborum at
+                accusantium nostrum cupiditate praesentium tempore qui vitae
+                neque? Fugiat aspernatur deleniti assumenda, at voluptates
+                eligendi adipisci et cum aut accusantium.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   );
 };
 
 export default CustomerService;
-
