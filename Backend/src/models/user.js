87c974cc63bce5cd051c5bd3bc4d9b789a93dc21
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-05-30 01:10:12 +0200

    Tagit bort kommentarer från user.js

--- Backend/src/models/user.js
+++ Backend/src/models/user.js
@@ -1,20 +1,11 @@
 
 const mongoose = require("mongoose");
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/userDB")
-// .then(() => console.log("Anslutning till MongoDB lyckades"))
-// .catch((err) => {
-//   console.error("Anslutning till MongoDB misslyckades: ", err);
-//   process.exit(1); // Avsluta processen om anslutningen misslyckades
-// });
-
-
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
-//   lastLogin: { type: Date },
 });
 
 const User = mongoose.model("userDB", userSchema);
