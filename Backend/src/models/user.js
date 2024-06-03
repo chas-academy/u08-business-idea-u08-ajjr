c47be046ead1694660a58e37a01addb4417ce4ff
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-06-03 02:19:43 +0200

    lagt till en role fieal för userkontroll

--- Backend/src/models/user.js
+++ Backend/src/models/user.js
@@ -1,10 +1,10 @@
-
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role: { type: String, default: "user" }, //  role field
   createdAt: { type: Date, default: Date.now },
 });
 
