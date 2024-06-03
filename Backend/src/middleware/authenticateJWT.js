07bccd3b769c2e42fd72b0696be55679dd57072e
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-06-03 21:37:27 +0200

    test 3

--- Backend/src/middleware/authenticateJWT.js
+++ Backend/src/middleware/authenticateJWT.js
@@ -1,9 +1,33 @@
+// const jwt = require("jsonwebtoken");
+// const User = require("../models/user");
+
+// const authenticateJWT = (req, res, next) => {
+//   console.log(req.cookies);
+//   const token = req.cookies.jwt_token;
+
+//   if (!token) {
+//     return res.status(401).json({ msg: "Ingen token, auktorisation nekad" });
+//   }
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+//     req.user = decoded;
+
+//     next();
+//   } catch (err) {
+//     res.status(401).json({ msg: "Token är inte giltig" });
+//   }
+// };
+
+// module.exports = authenticateJWT;
+
+
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
 
 const authenticateJWT = (req, res, next) => {
-  console.log(req.cookies);
-  const token = req.cookies.jwt_token;
+  console.log("Cookies: ", req.cookies);
+  const token = req.cookies.jwt_token || req.headers.authorization && req.headers.authorization.split(' ')[1];
 
   if (!token) {
     return res.status(401).json({ msg: "Ingen token, auktorisation nekad" });
@@ -11,6 +35,7 @@ const authenticateJWT = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("Decoded JWT: ", decoded);
 
     req.user = decoded;
 
