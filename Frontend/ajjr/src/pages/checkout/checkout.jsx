6f0e672009dd833fa71f9df8641d7bfc09a37df1
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-06-03 19:19:21 +0200

    fixat url för myaccount

--- Frontend/ajjr/src/pages/checkout/checkout.jsx
+++ Frontend/ajjr/src/pages/checkout/checkout.jsx
@@ -263,7 +263,7 @@ const Checkout = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:3000/api/orders", {
+      const response = await fetch("https://u08-business-idea-u08-ajjr-39gd.onrender.com/api/orders", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
