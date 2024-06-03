5fe6fe9aca4d84e8a424e458ab3b4fe7168ab291
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-06-03 02:25:58 +0200

    lagt till propsvalidation på children

--- Frontend/ajjr/src/components/adminroutes/AdminRoute.jsx
+++ Frontend/ajjr/src/components/adminroutes/AdminRoute.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import PropTypes from "prop-types";
 
 const AdminRoute = ({ children }) => {
   const isLoggedIn = localStorage.getItem("isLoggedIn");
   const userRole = localStorage.getItem("userRole");
-
 
   console.log("AdminRoute isLoggedIn:", isLoggedIn); 
   console.log("AdminRoute userRole:", userRole); 
@@ -14,6 +14,10 @@ const AdminRoute = ({ children }) => {
   }
 
   return children;
+};
+
+AdminRoute.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default AdminRoute;
