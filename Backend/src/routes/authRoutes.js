04232aa3a85ae59d983f74565f382fca3db29bc4
ArianAFK <arianfekri@hotmail.com>
2024-06-05 13:40:18 +0200

    samesite test

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -91,6 +91,7 @@ router.post("/login", async (req, res) => {
 
     res.cookie("jwt_token", token, {
       httpOnly: true,
+      sameSite: "none",
     }).json({ msg: "Du är inloggad", role: user.role });
   } catch (err) {
     res.status(500).json({ msg: "Serverfel" });
