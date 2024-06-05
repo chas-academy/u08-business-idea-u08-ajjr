3cc507f41f6dd342de50f3eddab563787d801dc9
Brianosumba <144438563+Brianosumba@users.noreply.github.com>
2024-06-05 21:02:51 +0200

    fix so that produkter show the produkter images

--- Frontend/ajjr/src/pages/perfumes/perfumes.jsx
+++ Frontend/ajjr/src/pages/perfumes/perfumes.jsx
@@ -1,29 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./perfumes.css";
-import product1 from "../../images/product1.jpg";
-import product2 from "../../images/product2.jpg";
-import product3 from "../../images/product3.jpg";
-import product4 from "../../images/product4.jpg";
-import product5 from "../../images/product5.jpg";
-import product6 from "../../images/product6.jpg";
-import product7 from "../../images/product7.jpg";
-import product8 from "../../images/product8.jpg";
-import product9 from "../../images/product9.jpg";
-import product10 from "../../images/product10.jpg";
-import product11 from "../../images/product11.jpg";
-import product12 from "../../images/product12.jpg";
-import product13 from "../../images/product13.jpg";
 import product14 from "../../images/product14.jpg";
-import product15 from "../../images/product15.jpg";
-import product16 from "../../images/product16.jpg";
-import product17 from "../../images/product17.jpg";
-import product18 from "../../images/product18.jpg";
 import testers from "../../images/testers.jpg";
 import { Link, useOutletContext, useSearchParams } from "react-router-dom";
 
 const Perfumes = () => {
-  const [products, getProducts] = useState([]);
+  const [products, setProducts] = useState([]);
   const { addToCart } = useOutletContext();
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
@@ -32,13 +15,14 @@ const Perfumes = () => {
   );
 
   useEffect(() => {
-    if (searchParams.get("category")) {
-      setActiveFilter(searchParams.get("category"));
+    if (activeFilter) {
+      setFilteredProducts(
+        products.filter((product) => product.category === activeFilter)
+      );
+    } else {
+      setFilteredProducts(products);
     }
-    setFilteredProducts(
-      products.filter((product) => product.category === activeFilter)
-    );
-  }, [searchParams, activeFilter]);
+  }, [products, activeFilter]);
 
   useEffect(() => {
     fetchProducts();
@@ -57,14 +41,14 @@ const Perfumes = () => {
       const data = await response.json();
 
       if (Array.isArray(data)) {
-        getProducts(data);
+        setProducts(data);
       } else {
-        getProducts([]);
-        console.error("Fetched orders are not an array:", data);
+        setProducts([]);
+        console.error("Fetched products are not an array:", data);
       }
     } catch (err) {
-      console.error("Failed to fetch orders:", err);
-      getProducts([]);
+      console.error("Failed to fetch products:", err);
+      setProducts([]);
     }
   };
 
@@ -131,14 +115,6 @@ const Perfumes = () => {
               {filteredProducts.map((product) => (
                 <div className="col" key={product._id}>
                   <div className="card product-card">
-                    {/*   <Link to="/productdetail">
-               <img
-                src={product.image}
-                className="card-img-top"
-                alt={product.name}
-              />
-            </Link> */}
-
                     <Link to="/productdetail">
                       <img
                         src={`${import.meta.env.VITE_API_URL}/` + product.image}
